7479afe6d3c710737638cad992d67eb796c4f14c
1642027 <chua.anime@gmail.com>
2017-10-20 23:38:01 +0900

    Create

---
+++ Bài 1/1642027/Source/1642027-BTCN01/1642027-BTCN01/DataProvider.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace _1642027_BTCN01
+{
+    class DataProvider
+    {
+        public DataTable HienThiHocSinh()
+        {
+            SqlConnection cn = new SqlConnection("Server=.;Database=DemoHocSinh;Trusted_Connection=True; ");
+            DataTable dt = new DataTable();
+            string sql = "select * from HocSinh ";
+            SqlDataAdapter da;
+            da = new SqlDataAdapter(sql, cn);
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+        }
+        public  int ThemHocSinh(String Ten, String NgaySinh, String Lop)
+        {
+            SqlConnection cn = new SqlConnection("Server=.;Database=DemoHocSinh;Trusted_Connection=True; ");
+            cn.Open();
+            string sql = string.Format("Insert into HocSinh(TenHocSinh,NgaySinh,Lop) values('{0}','{1}','{2}')",Ten,NgaySinh,Lop);
+            SqlCommand cm = new SqlCommand(sql, cn);
+            int rs = -1;
+            rs = cm.ExecuteNonQuery();
+            cn.Close();
+            return rs;
+        }
+        public  int XoaHocSinh(String Ma)
+        {
+            SqlConnection cn = new SqlConnection("Server=.;Database=DemoHocSinh;Trusted_Connection=True;");
+            cn.Open();
+            string sql = string.Format("delete from HocSinh where MaHocSinh='{0}'", Ma);
+            SqlCommand cm = new SqlCommand(sql, cn);
+            int rs = -1;
+            rs = cm.ExecuteNonQuery();
+            cn.Close();
+            return rs;
+        }
+        public  int UpdateHocSinh(String Ma,String Ten,String NgaySinh,String Lop)
+        {
+            SqlConnection cn = new SqlConnection("Server=.;Database=DemoHocSinh;Trusted_Connection=True;");
+            cn.Open();
+            string sql = string.Format("update HocSinh Set TenHocSinh=N'{0}', NgaySinh='{1}', Lop='{2}' where MaHocSinh='{3}' ",Ten,NgaySinh,Lop,Ma);
+            SqlCommand cm = new SqlCommand(sql, cn);
+            int rs = -1;
+            rs = cm.ExecuteNonQuery();
+            cn.Close();
+            return rs;
+        }
+        public DataTable TimKiemTheoTen(String Ten)
+        {
+            SqlConnection cn = new SqlConnection("Server=.;Database=DemoHocSinh;Trusted_Connection=True; ");
+            DataTable dt = new DataTable();
+            string sql = "select * from HocSinh WHERE TenHocSInh LIKE '%"+ Ten +"%'; ";
+            SqlDataAdapter da;
+            da = new SqlDataAdapter(sql, cn);
+            da.Fill(dt);
+            cn.Close();
+            return dt;
+        }
+    }
+}
---
+++ Bài 1/1642027/Source/1642027-BTCN01/1642027-BTCN01/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _1642027_BTCN01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Load();
+        }
+        public void Load()
+        {
+            DataProvider dp = new DataProvider();
+            dataGridView1.DataSource = dp.HienThiHocSinh();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataProvider dp = new DataProvider();
+            int kq = dp.ThemHocSinh(textBox2.Text, dateTimePicker1.Value.ToString(),comboBox1.Text);
+            if (kq > 0)
+            {
+                MessageBox.Show("Them Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Load();
+            }
+            else
+            {
+                MessageBox.Show("Them Khong Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataProvider dp = new DataProvider();
+            int kq = dp.XoaHocSinh(textBox1.Text);
+            if (kq > 0)
+            {
+                MessageBox.Show("Xoa Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Load();
+            }
+            else
+            {
+                MessageBox.Show("Xoa Khong Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+            textBox1.Text = dataGridView1.Rows[index].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[index].Cells[1].Value.ToString();
+            dateTimePicker1.Text = dataGridView1.Rows[index].Cells[2].Value.ToString();
+            comboBox1.Text = dataGridView1.Rows[index].Cells[3].Value.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataProvider dp = new DataProvider();
+            int kq = dp.UpdateHocSinh(textBox1.Text, textBox2.Text, dateTimePicker1.Value.ToString(), comboBox1.Text);
+            if(kq > 0)
+            {
+                MessageBox.Show("Sua Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Load();
+            }
+            else
+            {
+                MessageBox.Show("Sua Khong Thanh Cong", "Thong Tin", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DataProvider dp = new DataProvider();
+            dataGridView1.DataSource = dp.TimKiemTheoTen(textBox2.Text);
+        }
+    }
+}
